78c5477387ea89abb04202027897937e59ff90a3
Andrusha <robot@ev3dev:myproject>
2026-06-19 19:54:21 +0300

    renamed variables and functions

--- TrainControllerV2.py
+++ TrainControllerV2.py
@@ -13,277 +13,274 @@ import json
 ip = "192.168.1.185"
 port = 9600
 
-thr = 0
-thrmin = 0
-thrmax = 0
-brk = 0
-brkmin = 0
-brkmax = 0
-brkloc = 0
-brklocmin = 0
-brklocmax = 0
-rev = 0
+thrust = 0
+thrust_maximum = 0
+brake = 0
+brake_maximum = 0
+brake_locomotive = 0
+brake_locomotive_maximum = 0
+reverse = 0
 sand = 0
 
-freq = 0
-type = 0
-usedisp = 0
-usehost = 0
-usebut = 0
+frequency = 0
+data_type = 0
+use_display = 0
+setup_1 = 0
+setup_2 = 0
 config = {'freq': 0, 'type': 0, 'usedisp': 0, 'usehost': 0, 'usebut': 0}
 image = 'None'
-imagenew = 'DE2'
-counter = 5 # how many times sensors will be checked if it gives None
-data = {rev: 0, thr: 0, brk: 0, brkloc: 0, sand: 0}
+image_new = 'DE2'
+sensor_attempts = 5 # how many times sensors will be checked if it gives None
 
-sock = socket.socket()
-display = Display()
-but = Button()
-revsen = ColorSensor()
-thrmot = MediumMotor(OUTPUT_B)
-brklocmot = LargeMotor(OUTPUT_C)
-brkmot = LargeMotor(OUTPUT_D)
-snd = TouchSensor(INPUT_2)
-revsen.MODE_COL_COLOR # noqa
+Socket = socket.socket()
+Display = Display()
+Button = Button()
+ReverseSensor = ColorSensor()
+ThrustSensor = MediumMotor(OUTPUT_B)
+BrakeLocomotiveSensor = LargeMotor(OUTPUT_C)
+BrakeSensor = LargeMotor(OUTPUT_D)
+SandSensor = TouchSensor(INPUT_2)
+ReverseSensor.MODE_COL_COLOR # noqa
 
 def connect():
     global ip, port
     if ip == "":
         ip = input("Enter IP: ")
-        if port == 0:
-            port = input("Enter port: ")
+    if port == 0:
+        port = input("Enter port: ")
     try:
         print("Trying to connect")
-        sock.connect((ip, port))
+        Socket.connect((ip, port))
         print("Connected")
         return 1
     except Exception as e:
         print("Connection broken, error: {}".format(e)) # , press enter to reconnect or backspace to exit
-        exit()
-        # sock.close()
-        # while but.enter == False:
-        #     if but.backspace == True:
-        #         exit()
-        #     time.sleep(0.01)
-        # connect()
+        Socket.close()
+        while not Button.enter:
+            if Button.backspace:
+                exit()
+            time.sleep(0.01)
+        connect()
+        return None
+
 
 def meetup():        # !Call after calib()!
-    global freq, type, usedisp, usehost, usebut, config, maxvalues
+    global frequency, data_type, use_display, setup_1, setup_2, config, maximum_values
     connect()
-    config = sock.recv(1024)
+    config = Socket.recv(1024)
     config = config.decode('utf-8')
     print(config)
     config = json.loads(config)
     print(config)
-    freq = config['freq']
-    usedisp = config['usedisp']
-    usehost = config['usehost']
-    usebut = config['usebut']
-    type = config['type']
+    frequency = config['freq']
+    use_display = config['usedisp']
+    setup_1 = config['usehost']
+    setup_2 = config['usebut']
+    data_type = config['type']
     if config['type'] == 1:
-        sock.send(json.dumps(maxvalues).encode('utf-8'))
-    sock.send(bytes('ready', encoding='utf-8'))
+        Socket.send(json.dumps(maximum_values).encode('utf-8'))
+    Socket.send(bytes('ready', encoding='utf-8'))
 
 def calib():
-    global thrmax, brkmax, brklocmax, type, maxvalues
+    global thrust_maximum, brake_maximum, brake_locomotive_maximum, data_type, maximum_values
     print("Calibration, move all sliders down and press enter")
-    while but.enter == False:
+    while not Button.enter:
         time.sleep(0.001)
-    thrmot.position = 0
-    brkmot.position = 0
-    brklocmot.position = 0
-    while but.enter == True:
+    ThrustSensor.position = 0
+    BrakeSensor.position = 0
+    BrakeLocomotiveSensor.position = 0
+    while Button.enter:
         time.sleep(0.001)
     print("Now move all sliders up and press enter again")
-    while but.enter == False:
+    while not Button.enter:
         time.sleep(0.001)
-    thrmax = thrmot.position
-    brkmax = brkmot.position
-    brklocmax = brklocmot.position
-    while but.enter == True:
+    thrust_maximum = ThrustSensor.position
+    brake_maximum = BrakeSensor.position
+    brake_locomotive_maximum = BrakeLocomotiveSensor.position
+    while Button.enter:
         time.sleep(0.001)
-    maxvalues = {'thrmax': thrmax, 'brkmax': brkmax, 'brklocmax': brklocmax}
+    maximum_values = {'thrmax': thrust_maximum, 'brkmax': brake_maximum, 'brklocmax': brake_locomotive_maximum}
     print("Calibration done")
-    print(thrmax, brkmax, brklocmax)
+    print(thrust_maximum, brake_maximum, brake_locomotive_maximum)
 
-def getdata(a):
-    global thrmax, brkmax, brklocmax, rev, type, counter
-    c = 0
-    b = None
-    if a == 'thr':
-        while b == None:
-            b = thrmot.position
-            c = c + 1
-            if c == counter:
+def getdata(argument):
+    global thrust_maximum, brake_maximum, brake_locomotive_maximum, reverse, data_type, sensor_attempts
+    attempts_counter = 0
+    sensor_data = None
+    if argument == 'thr':
+        while sensor_data is None:
+            sensor_data = ThrustSensor.position
+            attempts_counter = attempts_counter + 1
+            if attempts_counter == sensor_attempts:
                 return 101
-        if type == 0:
-            if b == 0:
+        if data_type == 0:
+            if sensor_data == 0:
                 return 0
-            b = round((b / thrmax) * 100)
-            if b > 100:
+            sensor_data = round((sensor_data / thrust_maximum) * 100)
+            if sensor_data > 100:
                 return 100
-            if b < 0:
+            if sensor_data < 0:
                 return 0
             else:
-                return b
+                return sensor_data
         else:
-            return b
-    elif a == 'brk':
-        while b == None:
-            b = brkmot.position
-            c = c + 1
-            if c == counter:
+            return sensor_data
+    elif argument == 'brk':
+        while sensor_data is None:
+            sensor_data = BrakeSensor.position
+            attempts_counter = attempts_counter + 1
+            if attempts_counter == sensor_attempts:
                 return 102
-        if type == 0:
-            if b == 0:
+        if data_type == 0:
+            if sensor_data == 0:
                 return 0
-            b = round(b / brkmax * 100)
-            if b > 100:
+            sensor_data = round(sensor_data / brake_maximum * 100)
+            if sensor_data > 100:
                 return 100
-            if b < 0:
+            if sensor_data < 0:
                 return 0
             else:
-                return b
+                return sensor_data
         else:
-            return b
-    elif a == 'brkloc':
-        while b == None:
-            b = brklocmot.position
-            c = c + 1
-            if c == counter:
+            return sensor_data
+    elif argument == 'brkloc':
+        while sensor_data is None:
+            sensor_data = BrakeLocomotiveSensor.position
+            attempts_counter = attempts_counter + 1
+            if attempts_counter == sensor_attempts:
                 return 103
-        if type == 0:
-            if b == 0:
+        if data_type == 0:
+            if sensor_data == 0:
                 return 0
-            b = round(b / brklocmax * 100)
-            if b > 100:
+            sensor_data = round(sensor_data / brake_locomotive_maximum * 100)
+            if sensor_data > 100:
                 return 100
-            if b < 0:
+            if sensor_data < 0:
                 return 0
             else:
-                return b
+                return sensor_data
         else:
-            return b
-    elif a == 'but':
-        b = but.enter
-        b = b << 1
-        b = (b & but.up) << 1
-        b = (b & but.right) << 1
-        b = (b & but.down) << 1
-        b = (b & but.left) << 1
-        b = b & but.backspace
-        print(b)
-        return b
-    elif a == 'rev':
-        while b == None:
-            b = revsen.color
-            c = c + 1
-            if c == counter:
+            return sensor_data
+    elif argument == 'but':
+        sensor_data = Button.enter
+        sensor_data = sensor_data << 1
+        sensor_data = (sensor_data & Button.up) << 1
+        sensor_data = (sensor_data & Button.right) << 1
+        sensor_data = (sensor_data & Button.down) << 1
+        sensor_data = (sensor_data & Button.left) << 1
+        sensor_data = sensor_data & Button.backspace
+        print(sensor_data)
+        return sensor_data
+    elif argument == 'rev':
+        while sensor_data is None:
+            sensor_data = ReverseSensor.color
+            attempts_counter = attempts_counter + 1
+            if attempts_counter == sensor_attempts:
                 return 104
-        if b == 3:
+        if sensor_data == 3:
             return 2
-        elif b == 1:
+        elif sensor_data == 1:
             return 1
-        elif b == 5:
+        elif sensor_data == 5:
             return 0
         else:
             return 4
-    elif a == 'all':
-        if type == 1:
-            while b == None:
-                b = thrmot.position
-                c = c + 1
-                if c == counter:
-                    b = 101
-                    c = 0
-            thr = b
-            b = None
-            while b == None:
-                b = brkmot.position
-                c = c + 1
-                if c == counter:
-                    b = 102
-                    c = 0
-            brk = b
-            b = None
-            while b == None:
-                b = brklocmot.position
-                c = c + 1
-                if c == counter:
-                    b = 103
-                    c = 0
-            brkloc = b
-            b = None
-            while b == None:
-                b = revsen.color
-                c = c + 1
-                if c == counter:
-                    b = 104
-                    c = 0
-            return {'thr': thr,'brk': brk,'brkloc': brkloc,'rev': b, 'sand': snd.is_pressed}
-        else:
-            while b == None:
-                b = thrmot.position
-                c = c + 1
-                if c == counter:
-                    b = 101
-                    c = 0
-            thr = round((b / thrmax) * 100)
-            b = None
-            while b == None:
-                b = brkmot.position
-                c = c + 1
-                if c == counter:
-                    b = 102
-                    c = 0
-            brk = round((b / brkmax) * 100)
-            b = None
-            while b == None:
-                b = brklocmot.position
-                c = c + 1
-                if c == counter:
-                    b = 103
-                    c = 0
-            brkloc = round((b / brklocmax) * 100)
-            b = None
-            while b == None:
-                b = revsen.color
-                c = c + 1
-                if c == counter:
-                    b = 104
-                    c = 0
-            if b == 3:
-                b = 2
-            elif b == 1:
-                b = 1
-            elif b == 5:
-                b = 0
-            else:
-                b = 4
-            return {'thr': thr,'brk': brk,'brkloc': brkloc,'rev': b, 'sand': snd.is_pressed}
+    # elif argument == 'all':
+    #     if data_type == 1:
+    #         while sensor_data is None:
+    #             sensor_data = ThrustSensor.position
+    #             attempts_counter = attempts_counter + 1
+    #             if attempts_counter == sensor_attempts:
+    #                 sensor_data = 101
+    #                 attempts_counter = 0
+    #         thrust = sensor_data
+    #         sensor_data = None
+    #         while sensor_data is None:
+    #             sensor_data = BrakeSensor.position
+    #             attempts_counter = attempts_counter + 1
+    #             if attempts_counter == sensor_attempts:
+    #                 sensor_data = 102
+    #                 attempts_counter = 0
+    #         brake = sensor_data
+    #         sensor_data = None
+    #         while sensor_data is None:
+    #             sensor_data = BrakeLocomotiveSensor.position
+    #             attempts_counter = attempts_counter + 1
+    #             if attempts_counter == sensor_attempts:
+    #                 sensor_data = 103
+    #                 attempts_counter = 0
+    #         brake_locomotive = sensor_data
+    #         sensor_data = None
+    #         while sensor_data is None:
+    #             sensor_data = ReverseSensor.color
+    #             attempts_counter = attempts_counter + 1
+    #             if attempts_counter == sensor_attempts:
+    #                 sensor_data = 104
+    #                 attempts_counter = 0
+    #         return {'thr': thrust,'brk': brake,'brkloc': brake_locomotive,'rev': sensor_data, 'sand': SandSensor.is_pressed}
+    #     else:
+    #         while sensor_data is None:
+    #             sensor_data = ThrustSensor.position
+    #             attempts_counter = attempts_counter + 1
+    #             if attempts_counter == sensor_attempts:
+    #                 sensor_data = 101
+    #                 attempts_counter = 0
+    #         thrust = round((sensor_data / thrust_maximum) * 100)
+    #         sensor_data = None
+    #         while sensor_data is None:
+    #             sensor_data = BrakeSensor.position
+    #             attempts_counter = attempts_counter + 1
+    #             if attempts_counter == sensor_attempts:
+    #                 sensor_data = 102
+    #                 attempts_counter = 0
+    #         brake = round((sensor_data / brake_maximum) * 100)
+    #         sensor_data = None
+    #         while sensor_data is None:
+    #             sensor_data = BrakeLocomotiveSensor.position
+    #             attempts_counter = attempts_counter + 1
+    #             if attempts_counter == sensor_attempts:
+    #                 sensor_data = 103
+    #                 attempts_counter = 0
+    #         brake_locomotive = round((sensor_data / brake_locomotive_maximum) * 100)
+    #         sensor_data = None
+    #         while sensor_data is None:
+    #             sensor_data = ReverseSensor.color
+    #             attempts_counter = attempts_counter + 1
+    #             if attempts_counter == sensor_attempts:
+    #                 sensor_data = 104
+    #                 attempts_counter = 0
+    #         if sensor_data == 3:
+    #             sensor_data = 2
+    #         elif sensor_data == 1:
+    #             sensor_data = 1
+    #         elif sensor_data == 5:
+    #             sensor_data = 0
+    #         else:
+    #             sensor_data = 4
+    #         return {'thr': thrust,'brk': brake,'brkloc': brake_locomotive,'rev': sensor_data, 'sand': SandSensor.is_pressed}
     print('getdata: Wrong argument')
     return None
 
 def screenupdate():
-    global image, imagenew, start
+    global image, image_new, start
     while True:
-        display.update()
+        Display.update()
         if time.perf_counter() - start > 1:
             start = time.perf_counter()
-            ready, _, _ = select.select([sock], [], [], 0)
-            display.update()
+            ready, _, _ = select.select([Socket], [], [], 0)
+            Display.update()
             if ready:
-                imagenew = sock.recv(256).decode('utf-8')
-                if image != imagenew:
-                    print("Image changed from", image, 'to', imagenew)
-                    image = imagenew
+                image_new = Socket.recv(256).decode('utf-8')
+                if image != image_new:
+                    print("Image changed from", image, 'to', image_new)
+                    image = image_new
                     if image != 'None':
                         try:
                             imagedisplay = Image.open("/home/robot/TrainControllerV2/Pictures/{}.bmp".format(image))
-                            display.clear()
-                            display.update()
-                            display.image.paste(imagedisplay, (0, 0))
-                            display.update()
+                            Display.clear()
+                            Display.update()
+                            Display.image.paste(imagedisplay, (0, 0))
+                            Display.update()
                         except FileNotFoundError:
                             print("image not found: {}".format(image))
                         except Exception as e:
@@ -296,27 +293,27 @@ start = time.perf_counter()
 calib()
 meetup()
 
-if usedisp == 1:
+if use_display:
     screen_thread = threading.Thread(target=screenupdate, daemon=True)
     screen_thread.start()
 
-while but.backspace == False:
+while not Button.backspace:
     data = json.dumps({'thr': getdata('thr'),
                        'brk': getdata('brk'),
                        'brkloc': getdata('brkloc'),
                        'rev': getdata('rev'),
-                       'sand': snd.is_pressed,
-                       'butleft': but.left,
-                       'butup': but.up,
-                       'butright': but.right,
-                       'butdown': but.down,
-                       'butenter': but.enter}) + '\n'
+                       'sand': SandSensor.is_pressed,
+                       'butleft': Button.left,
+                       'butup': Button.up,
+                       'butright': Button.right,
+                       'butdown': Button.down,
+                       'butenter': Button.enter}) + '\n'
     try:
-        sock.sendall(data.encode('utf-8'))
+        Socket.sendall(data.encode('utf-8'))
     except Exception as e:
         print("Error occurred: {}".format(e))
         connect()
-    time.sleep(round(1 / freq, 4))
-sock.detach()
-sock.close()
+    time.sleep(round(1 / frequency, 4))
+Socket.detach()
+Socket.close()
 exit() #
